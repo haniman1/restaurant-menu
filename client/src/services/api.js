352a8836d60cc3e282adfa998863bc39c2f244fd
haniman1 <hanielabate@gmail.com>
2026-06-18 18:12:12 +0300

    1

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://restaurant-menu-lney.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 // ADD TOKEN AUTOMATICALLY
